c9672d733f55ea7e0584e3da296d3a245acd29d9
Akasha <Akasha.Rojee@gmail.com>
2021-08-05 14:56:40 +0400

    Implement server-side authentication

--- src/js/MarvelAPI.js
+++ src/js/MarvelAPI.js
@@ -3,8 +3,7 @@ import API from './API.js';
 export default class MarvelAPI extends API {
   
   async getCharacters() {
-
-    super.url = new URL('http://gateway.marvel.com/v1/public/characters');
+    super.url = new URL('https://gateway.marvel.com/v1/public/characters');
 
     super.params = {
       apikey: '86af4a1749d744f4f6c643c5c87a4f3c',
